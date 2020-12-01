616a79c0ed14bc8624375111ee1c5ff127df6382
lisalluo <35357860+lisalluo@users.noreply.github.com>
2020-12-01 14:04:26 -0800

    Clean up backup.sh

--- backup.sh
+++ backup.sh
@@ -11,14 +11,14 @@ S3_BUCKET=$S3_BUCKET
 
 S3PATH="s3://$S3_BUCKET/$DB/"
 DATE=`date -u +"%Y-%m-%dT%H%M%SZ"`
-# S3BACKUP=$S3PATH`date -u +"%Y-%m-%dT%H%M%SZ"`.dump.gz
 S3BACKUP=$S3PATH$DATE.dump.gz
 S3LATEST=$S3PATH"latest".dump.gz
 /usr/bin/aws s3 mb $S3PATH
 
-# /usr/bin/mongodump -h $HOST -d $DB -u $USER -p $PASSWORD --gzip --archive | aws s3 cp - $S3BACKUP
-# /usr/bin/mongodump --uri $URI --gzip --archive | aws s3 cp - $S3BACKUP
+# Create dump and save in file $DATE.dump.gz
 /usr/bin/mongodump --uri $URI --gzip --archive=$DATE.dump.gz
+
+# Upload new dump to s3 as $Date.dump.gz AND latest.dump.gz
 aws s3 cp $DATE.dump.gz $S3BACKUP
 aws s3 cp $S3BACKUP $S3LATEST
 
