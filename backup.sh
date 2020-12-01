46561724a71198b0fc7b484bf13a83016abc15e0
lisalluo <35357860+lisalluo@users.noreply.github.com>
2020-11-30 23:45:43 -0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,12 +10,16 @@ URI="mongodb://$USER:$PASSWORD@$HOST/$DB"
 S3_BUCKET=$S3_BUCKET
 
 S3PATH="s3://$S3_BUCKET/$DB/"
-S3BACKUP=$S3PATH`date -u +"%Y-%m-%dT%H%M%SZ"`.dump.gz
+DATE=`date -u +"%Y-%m-%dT%H%M%SZ"`
+# S3BACKUP=$S3PATH`date -u +"%Y-%m-%dT%H%M%SZ"`.dump.gz
+S3BACKUP=$S3PATH$DATE.dump.gz
 S3LATEST=$S3PATH"latest".dump.gz
 /usr/bin/aws s3 mb $S3PATH
 
 # /usr/bin/mongodump -h $HOST -d $DB -u $USER -p $PASSWORD --gzip --archive | aws s3 cp - $S3BACKUP
-/usr/bin/mongodump --uri $URI --gzip --archive | aws s3 cp - $S3BACKUP
+# /usr/bin/mongodump --uri $URI --gzip --archive | aws s3 cp - $S3BACKUP
+/usr/bin/mongodump --uri $URI --gzip --archive=$DATE.dump.gz
+aws s3 cp $DATE.dump.gz $S3BACKUP
 aws s3 cp $S3BACKUP $S3LATEST
 
 # Restore
